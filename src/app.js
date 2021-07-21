60e4c5df640ecf3bf8a541cbe3497e33c4b206a3
Jonah Tarver <jonahdevine@gmail.com>
2021-07-21 14:29:05 -0700

    Create POST /success and prep for db

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+const { NODE_ENV, CLIENT_ORIGIN } = require('./config')
 const jsonParser = express.json()
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
@@ -15,43 +15,60 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
 
 app.post('/create-checkout-session', jsonParser, async (req, res) => {
-    const {products} = req.body
-
-    console.log(products)
+    const {products, pickingUp} = req.body
+    const over100 = products.map(p => p.unit_amount).reduce((a, c) => a+c)/100 >= 10000
+    const shippingDetails = !pickingUp && {
+        shipping_address_collection: {
+            allowed_countries: ['US']
+        },
+        shipping_rates: over100 ? ['shr_1JFTdVHbvDyG32Z94gjal3wd'] : ['shr_1JFTCOHbvDyG32Z9Ib4cpWZY']
+    }
     const stripeProducts = await Promise.all(products.map(async p => {
         const {name, image} = p
         const product = await stripe.products.create({
             name,
-            images: ['https:' + image]
+            images: [image]
         });
         return product
     }))
     const stripePrices = await Promise.all(
         products.map(async (p,i) => {
             const stripePrice = await stripe.prices.create({
-                unit_amount: 2000,
+                unit_amount: p.unit_amount,
                 currency: 'usd',
                 product: stripeProducts[i].id,
             })
             return {price: stripePrice.id, quantity: products[i].quantity}
         })
-    ) 
+    )
     const session = await stripe.checkout.sessions.create({
-        success_url: 'https://example.com/success',
-        cancel_url: 'https://example.com/cancel',
+        success_url: 'http://localhost:8000/success?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url: 'http://localhost:8000/cart/',
         payment_method_types: ['card'],
         line_items: stripePrices,
         mode: 'payment',
+        ...shippingDetails
     });
     res.send(session)
 })
+
+app.post('/success', jsonParser, async (req, res) => {
+    const session = await stripe.checkout.sessions.retrieve(req.body.session_id);
+    const customer = await stripe.customers.retrieve(session.customer);
+  
+    res.send({session, customer});
+});
 
 app.use((error, req, res, next) => {
     let response
--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 5432,
     NODE_ENV: process.env.NODE_ENV || 'development',
+    DATABASE_URL: process.env.DATABASE_URL || "postgresql://jonahtarver@localhost/rotation",
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://jonahtarver@localhost/rotation-test",
+    CLIENT_ORIGIN: "http://localhost:3000" //process.env.CLIENT_ORIGIN || "https://featherreport.biz"
 }
